f5a9846522cd5a726f7090e96da0953a13ab9779
Vladislav Radonjic <vanjar@gmail.com>
2019-01-07 21:21:50 +0000

    Added destroy action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -38,4 +38,15 @@ class PortfoliosController < ApplicationController
   def show
   	@portfolio_item = Portfolio.find(params[:id])
   end
+
+  def destroy
+  	# lookup
+  	@portfolio_item = Portfolio.find(params[:id])
+  	# delete
+  	@portfolio_item.destroy
+  	#redirect
+  	respond_to do |f|
+  		f.html { redirect_to portfolios_url, notice: "Record was removed." }
+  	end
+  end
 end
