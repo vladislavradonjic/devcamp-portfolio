85f19fef2391540bae59a74e09e96f9a732e212f
Vladislav Radonjic <vanjar@gmail.com>
2019-01-16 21:27:29 +0000

    implementing defaults

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -7,4 +7,12 @@ class Portfolio < ApplicationRecord
 	end
 
 	scope :ruby_on_rails_portfolio_item, -> {where(subtitle: 'Ruby on Rails')}
+
+	after_initialize :set_defaults
+
+	def set_defaults
+		self.main_image ||= "http://placehold.it/600x400"
+		self.thumb_image ||= "http://placehold.it/350x200"
+	end
+
 end
