719e20b49268f62a76f15e00c0da4ea489fa46f6
Vladislav Radonjic <vanjar@gmail.com>
2019-01-16 22:31:31 +0000

    implemented concern, but not really, commented it out

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,10 @@
 class Skill < ApplicationRecord
+	#include Placeholder
 	validates_presence_of :title, :percent_utilized
+
+	after_initialize :set_defaults
+
+	def set_defaults
+		self.badge ||= "http://placehold.it/250x250"
+	end
 end
