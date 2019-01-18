102f8611f8049fdd81d6e228c6ab367fbc8fe576
Vladislav Radonjic <vanjar@gmail.com>
2019-01-18 04:02:22 +0000

    implemented virtual atributes in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :trackable
+
+  validates_presence_of :name
+
+  def first_name
+  	self.name.split.first
+  end
+
+  def last_name
+  	self.name.split.last
+  end
+
 end
