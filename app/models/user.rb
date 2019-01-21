ef1e9850465096b0660cdb379e4d78ee3582f80b
Vladislav Radonjic <vanjar@gmail.com>
2019-01-21 22:53:59 +0000

    added installation requirements for petergate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
+  ############################################################################################
+  ## PeterGate Roles                                                                        ##
+  ## The :user role is added by default and shouldn't be included in this list.             ##
+  ## The :root_admin can access any page regardless of access settings. Use with caution!   ##
+  ## The multiple option can be set to true if you need users to have multiple roles.       ##
+  petergate(roles: [:admin, :editor], multiple: false)                                      ##
+  ############################################################################################ 
+ 
+
   # Include default devise modules. Others available are:
   # :confirmable, #ovo znaci validacija sign upa putem emaila
   #:lockable, #lock a user out based on amount of wrong user/password pairs
