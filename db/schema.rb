4474cebf769088450f2b48a4ba279ec5b4964023
Vladislav Radonjic <vanjar@gmail.com>
2019-01-30 21:16:55 +0000

    portfolio has postion attr

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_21_224814) do
+ActiveRecord::Schema.define(version: 2019_01_29_001031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2019_01_21_224814) do
     t.text "thumb_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "position"
   end
 
   create_table "skills", force: :cascade do |t|
